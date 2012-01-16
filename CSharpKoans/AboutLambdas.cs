48e7161d5920953b35d90d69bcf00e7cc0d0bf3a
Marina Fedner <marina.fedner@gmail.com>
2012-01-16 14:53:44 -0500

    add lambdas koans

--- CSharpKoans/AboutLambdas.cs
+++ CSharpKoans/AboutLambdas.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using CSharpKoans.Core;
 using NUnit.Framework;
+using System.Collections;
 
 namespace CSharpKoans
 {
     public class AboutLambdas : KoanContainer
     {
         const int ___ = int.MaxValue;
-        const KoanDelegate FILLMEIN = n => n;
-
+        KoanDelegate FILLMEIN = null;// n => n;
+         BooleanKoanDelegate BOOLFILLMEIN =null; //= n => false;
+        Func<int, bool> PREDICATE = null;
       
 
         /*  A lambda expression is an anonymous function that can contain expressions and statements
@@ -26,22 +28,79 @@ namespace CSharpKoans
         {
             /* note the definition of KoanDelegate above! */
             KoanDelegate timesTwo = n=> n*2;
-            var result = timesTwo(5);
+            int result = timesTwo(5);
 
             Assert.AreEqual(___, result);
 
             /* replace FILLMEIN with code to create a delegate that adds 5 to its argument */
             KoanDelegate plusFive = FILLMEIN;
-            var additionResult = plusFive(3);
+            int additionResult = plusFive(3);
             Assert.AreEqual(8, additionResult);
             
         }
 
+        delegate bool BooleanKoanDelegate(string s);
 
+        [Koan]
+        public void DelegatesCanHaveVariousArgumentAndReturnTypes()
+        {
+
+            /* replace BOOLFILLMEIN with a lambda to create a delegate that returns true if the # of chars in the argument is > five, false otherwise */
+            BooleanKoanDelegate isLongerThanFive = BOOLFILLMEIN;
+            bool result = isLongerThanFive("srtsolutions");
+            
+            bool no = isLongerThanFive("a2");
+            Assert.True(result);
+            Assert.IsFalse(no);
+
+        }
+
+        [Koan]
+        public void FuncsAreDelegatesWithTypeArguments()
+        {
+
+            Func<int, bool> myFunc = x => x > 5;
+            bool result = myFunc(4);
+            Assert.AreEqual(___, result);
+
+            /* fill in an appropriate argument so that the return value is true */
+            bool makeThisTrue = myFunc(___);
+            Assert.True(makeThisTrue);
+        }
+
+        [Koan]
         public void LambdasCanBeUsedWithStandardQueryOperators()
-        { }
+        {
+            int[] numbers = { 1,9,7,3,6,8,5};
+            var smallNumbers = numbers.Where(n => n< 4);
+            Assert.AreEqual(___, smallNumbers.Count());
+          
+            /* replace PREDICATE with a lambda that returns true if a number is even */
+            IEnumerable<int> evenNumbers = numbers.Where(PREDICATE);
+            Assert.AreEqual(2, evenNumbers.Count());
+        
+        }
 
-     
+        [Koan]
+        public void CountQueryOperatorCanTakeALambda()
+        {
+            int[] numbers = { 1, 9, 7, 3, 6, 8, 5 };
+            Assert.AreEqual(___, numbers.Count());
+
+            /* replace PREDICATE with a lambda that returns true if a number is >7 */
+            int largeNumbers = numbers.Count(PREDICATE);
+            Assert.AreEqual(2, largeNumbers);
+        }
+
+        [Koan]
+        public void GreaterThanOrEqualToSymbolDiffersFromGoesTo()
+        {
+            int[] numbers = { 1, 9, 7, 3, 6, 8, 5 };
+
+            /* replace PREDICATE with a lambda that returns true if a number is >= 7 */
+            int largeNumbers = numbers.Count(PREDICATE);
+            Assert.AreEqual(3, largeNumbers);
+        }
     }
 
 
--- CSharpKoans/PathToEnlightenment.cs
+++ CSharpKoans/PathToEnlightenment.cs
@@ -14,33 +14,14 @@ namespace CSharpKoans
             get
             {
                 return new List<KoanContainer>() { 
-                new AboutAsserts() , 
-              new AboutLambdas(),
-                new AboutLinq()
-                 };
+                    new AboutAsserts() , 
+                    new AboutLambdas(),
+                    new AboutLinq()
+                };
             }
         }
-        //let (containers: obj list) = [ ``about asserts``(); 
-        //                               ``about let``();
-        //                               ``about functions``();
-        //                               ``about the order of evaluation``();
-        //                               ``about unit``();
-        //                               ``about tuples``();
-        //                               ``about strings``();
-        //                               ``about branching``();
-        //                               ``about lists``();
-        //                               ``about pipelining``();
-        //                               ``about arrays``();
-        //                               ``about dot net collections``();
-        //                               ``about looping``();
-        //                               ``more about functions``();
-        //                               ``about record types``();
-        //                               ``about option types``();
-        //                               ``about descriminated unions``();
-        //                               ``about modules``();
-        //                               ``about classes``();
-        //                               ]
-     
+
+
 
         public void Execute()
         {
@@ -68,25 +49,6 @@ namespace CSharpKoans
                 System.Console.ReadKey();
             }
         }
-        //match result with
-        //| Success message -> printf "%s" message
-        //| Failure (message, ex) -> 
-        //    printf "%s" message
-        //    printfn ""
-        //    printfn ""
-        //    printfn ""
-        //    printfn ""
-        //    printfn "You have not yet reached enlightenment ..."
-        //    printfn "%s" ex.Message
-        //    printfn ""
-        //    printfn "Please meditate on the following code:"
-        //    printfn "%s" ex.StackTrace
 
-        //printfn ""
-        //printfn ""
-        //printfn ""
-        //printfn ""
-        //printf "Press any key to continue..."
-        //System.Console.ReadKey() |> ignore
     }
 }
