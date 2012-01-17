f1000a0ce0158c2202a0d6afa017291fd9fe6b42
unknown <Bill Wagner@.(none)>
2012-01-17 15:34:37 -0500

    Add comments and asserts around returns, start thinking about more LINQ koans

--- CSharpKoans/AboutLinq.cs
+++ CSharpKoans/AboutLinq.cs
@@ -9,8 +9,6 @@ namespace CSharpKoans
 {
     public class AboutLinq : KoanContainer
     {
-        public const string __ = "FILL ME IN";
-
         public class FILLMEIN
         { }
         private bool ___()
@@ -33,10 +31,10 @@ namespace CSharpKoans
 
             var numbers = new List<int> { 1, 2, 3, 4, 5 };
             IEnumerable<int> evens = from n in numbers
-                           where n % 2 ==0
-                select n;
+                                     where n % 2 == 0
+                                     select n;
 
-            Assert.AreEqual(__, evens.Count());
+            Assert.AreEqual("Fill me in", evens.Count());
 
             /* now modify this query to get the odd numbers */
             IEnumerable<int> odds = null;
@@ -51,14 +49,13 @@ namespace CSharpKoans
             var numbers = new List<int> { 1, 2, 3, 4, 5 };
 
             /* instead of the function __(), put in the correct delegate of form n=> fn(n) */
-            IEnumerable<int> evens = numbers.Where(n=>___()).Select(n=>n);
+            IEnumerable<int> evens = numbers.Where(n=>true).Select(n=>n);
             Assert.AreEqual(2, evens.Count());
 
    
             /* you don't even need the Select() here */
-            IEnumerable<int> noSelect = numbers.Where(n=> ___());
+            IEnumerable<int> noSelect = numbers.Where(n=> true);
             Assert.AreEqual(2, noSelect.Count());
-
         }
 
         [Koan]
@@ -67,8 +64,8 @@ namespace CSharpKoans
 
             var numbers = new List<int> { 1, 2, 3, 4, 5 };
 
-            /* instead of the function __(), put in the correct function of form n=> fn(n), with return type bool */
-            var squaredEvens = numbers.Where(n => ___()).Select(n=> "Fill me in"); // need a cute way to have a 'FILL ME IN' function here
+            /* instead of the function true, put in the correct function of form n=> fn(n), with return type bool */
+            var squaredEvens = numbers.Where(n => true).Select(n=> "Fill me in"); 
             Assert.AreEqual(4, squaredEvens.First());
         }
 
@@ -77,11 +74,12 @@ namespace CSharpKoans
         {
             count = 0;
             var aSelectStatement = from b in Library.Books
-                               select incrementCount(b);
+                                   select CountMe(b);
 
+            // run another query to show that select produces a sequence.
             Assert.IsInstanceOf<FILLMEIN>(aSelectStatement, "What is the type of aSelectStatement?");
 
-            Assert.AreEqual(count, __, "How many times was incrementCount called?");
+            Assert.AreEqual(count, "Fill me in", "How many times was CountMe called?");
                 
         }
 
@@ -90,13 +88,15 @@ namespace CSharpKoans
         {
             count = 0;
             var aSelectStatement = from b in Library.Books
-                                   select incrementCount(b);
+                                   select CountMe(b);
+
+            Assert.AreEqual(count, "Fill me in", "How many times was CountMe called yet?");
 
             /* calling ToList executes the statement */
             var aSelectStatementExecuted = aSelectStatement.ToList();
-      
-           
-            Assert.AreEqual(count, __, "How many times was incrementCount called?");
+
+
+            Assert.AreEqual(count, "Fill me in", "How many times was CountMe called now?");
         
         }
 
@@ -106,21 +106,19 @@ namespace CSharpKoans
 
             count = 0;
             var books = from b in Library.Books
-                      select incrementCount(b);
+                      select CountMe(b);
 
             /*   Functions like Count (also: Average(), Max() ) execute without an explicit foreach statement, but foreach is used to iterate through the results (and this executes the statement)  */
            var numBooks= books.Count();
-           Assert.AreEqual(__, numBooks, "Just getting the number of books in our library.");
-            Assert.AreEqual(count, __, "How many times was incrementCount called?");
+           Assert.AreEqual("Fill me in", numBooks, "Just getting the number of books in our library.");
+           Assert.AreEqual(count, "Fill me in", "How many times was CountMe called?");
 
-        }
-
-     
+        }   
 
   
 
         [Koan]
-        public void OrderbyClauseChangesOrder()
+        public void OrderbyClauseSortsTheOutputSequence()
         {
             var defaultOrder = from b in Library.Books
                                select b.Title;
@@ -130,15 +128,13 @@ namespace CSharpKoans
             var byYear = from b in Library.Books
                          orderby b.PublicationYear
                          select b.Title;
-            Assert.AreEqual(__, byYear.First());
-
-
+            Assert.AreEqual("Fill me in", byYear.First());
 
         }
 
 
         public int count;
-        private Book incrementCount(Book b)
+        private T CountMe<T>(T b)
         {
             count++;
             return b;
