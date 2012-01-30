4cf210c4f2abc49289bcc207eaf42e9f1ec1e41e
Marina Fedner <marina.fedner@gmail.com>
2012-01-30 10:24:38 -0500

    more linq to xml koan improvements

--- CSharpKoans/AboutLinqToXml.cs
+++ CSharpKoans/AboutLinqToXml.cs
@@ -12,11 +12,12 @@ namespace CSharpKoans
     
     public class AboutLinqToXml :KoanContainer
     {
-        XDocument document = XDocument.Load(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "senators_cfm.xml"));
+        XDocument senatorsDoc = XDocument.Load(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "senators_cfm.xml"));
+        XDocument nutritionDoc = XDocument.Load(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "nutrition.xml"));
 
-
+        /* DO NOT CHANGE THESE VALUES. MAKE THE KOANS WORK BY REPLACING ___ OR FILLMEIN WITH THE APPROPRIATE CODE */
         const int ___ = int.MaxValue;
-        const string[] FILLMEIN = new string[] { };
+         string[] FILLMEIN = new string[] { } ;
 
         public class Senator
         {
@@ -25,29 +26,108 @@ namespace CSharpKoans
             public string Website { get; set; }
         }
 
-        [Koan]
-        public void YouCanUseLinqToRetrieveDataFromXmlDocuments()
+
+     //   [Koan]
+        public void LinqToXmlBuildsAnObjectTreeFromTheXmlDocument()
         {
-            var contactInfo = document.Root;
-            var senators = FILLMEIN;
-            /* write a query to find the list of senators (do the simplest thing that works, and leave them as XElements) */
-               
 
-            Assert.AreEqual(100, senators.Count());
-
-            /* write a line of code that gets the last_name of the third XElement in the "senators" list */
-            var thirdSenatorsName = "null";
-            Assert.AreEqual("Ayotte", thirdSenatorsName);
+            var contactInfo = senatorsDoc.Root;
 
 
+            /* contactInfo is the root node */
 
+            Assert.AreEqual(___, contactInfo.Name.LocalName);
+            Assert.AreEqual(___, contactInfo.Parent);
+            Assert.AreEqual(___, contactInfo.HasElements);
+
+            /* Elements() are the direct child nodes of a parent node */
+            Assert.AreEqual(___, contactInfo.Elements().Count());
+
+            /* Descendants are all the children at any level. */
+            Assert.AreEqual(___, contactInfo.Descendants().Count());
+        
+        }
+
+       // [Koan]
+        public void EachNodeInTheTreeIsAlsoATree()
+        {
+            var contactInfo = senatorsDoc.Root;
+
+            var senators = from s in contactInfo.Elements()
+                           select s;
+
+          //  Assert.AreEqual(___, senators.Count());
+          
+
+            var firstSenator = senators.First();
+
+
+            Assert.AreEqual(___, firstSenator.Name.LocalName);
+            Assert.AreEqual(___, firstSenator.Parent.Name.LocalName);
+            Assert.AreEqual(___, firstSenator.HasElements);
+
+            /* ancestors are the opposite of descendants */
+           Assert.AreEqual(___, firstSenator.Ancestors().Count());
+
+            /* Elements() are the direct child nodes of a parent node */
+            Assert.AreEqual(___, firstSenator.Elements().Count());
+
+            /* Descendants are all the children at any level. */
+            Assert.AreEqual(___, firstSenator.Descendants().Count());
+
+            /* searching for an child node with a specific name returns all matching child nodes */
+            var lastNameNodes = firstSenator.Elements("last_name");
+
+            Assert.AreEqual(___, lastNameNodes.Count());
+
+            var lastName = lastNameNodes.First();
+
+            /* Name vs. Value for a node */
+            Assert.AreEqual(___, lastName.Name.LocalName);
+            Assert.AreEqual(___, lastName.Value);
+
+      
+          
      
         }
 
         [Koan]
+        public void YouCanUseLinqToQueryElements()
+        {
+
+            var contactInfo = senatorsDoc.Root;
+            var senators = contactInfo.Elements();
+            /* OR: from var s contactInfo.Elements select s; */
+            var firstSenator = senators.First();
+
+            /* you could also query the elements using LINQ */
+            var state = from e in firstSenator.Elements()
+                        where e.Name == "state"
+                        select e.Value;
+
+
+            Assert.AreEqual(___, state.Count());
+            Assert.AreEqual(___, state.First());
+        }
+
+        [Koan]
+        public void LinqToXmlProducesEnumerableData()
+        {
+            var contactInfo = senatorsDoc.Root;
+            var nameOfThirdSenator = from s in contactInfo.Elements()
+                           // fill in query here
+                           select s;
+
+            /* write a line of code that gets the last_name of the third XElement in the "senators" list */
+            Assert.AreEqual("Ayotte", nameOfThirdSenator);
+
+        }
+
+
+        [Koan]
         public void LinqToXmlCreatesObjectsFromXmlElements()
         {
-            var contactInfo = document.Root;
+            var contactInfo = senatorsDoc.Root;
 
             /* write a LINQ query to put the senator xElements into Senator objects 
              hint: select new Senator() */
@@ -67,6 +147,31 @@ namespace CSharpKoans
             var orderedRepubs = from s in senatorObjects
                                 // write the correct query here
                                 select s;
+
+            /* don't change this: just make sure it passes */
+            Assert.IsInstanceOf<IEnumerable<Senator>>(orderedRepubs);
+
+            /* Write a query to find the state of the Independent senator.  (Party = ID) */
+            var state = "Replace this string with a LINQ to XML Query";
+
+            Assert.AreEqual(state, "CT");
+
+
+        }
+
+
+        public class Food
+        { 
+            /* you will need to fill this in */
+        }
+
+
+        [Koan]
+        public void YouCanMakeComplexQueriesIntoXmlFilesWithLinq()
+        {
+
+            var foods = nutritionDoc.Root;
+
 
         
         }
