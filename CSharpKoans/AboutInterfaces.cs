168f162db55803af8c2d8c92d040dee98b8e6154
Bill Wagner <bill.w.wagner@outlook.com>
2014-06-25 22:45:36 +0100

    Add first pass at about interfaces

--- CSharpKoans/AboutInterfaces.cs
+++ CSharpKoans/AboutInterfaces.cs
@@ -1,8 +1,43 @@
 using CSharpKoans.Core;
+using NUnit.Framework;
+using System.Text;
 
 namespace CSharpKoans
 {
-    internal class AboutInterfaces : KoanContainer
+    public  class AboutInterfaces : KoanContainer
     {
+        public interface ICharacter
+        {
+            string SayCatchPhrase();
+        }
+
+        private string RepeatCatchPhrase(ICharacter sidekick)
+        {
+            StringBuilder rVal = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+                rVal.Append(sidekick.SayCatchPhrase());
+
+            return rVal.ToString();
+        }
+
+        [Koan]
+        public void InterfacesProvideCommonContracts()
+        {
+            var Robin = new object();
+            var Pinky = new object();
+
+            // Write code so that you can call RepeatCatchPhrase for both Robin and Tonnto
+            // create different classes for a super hero side kick,
+            // and a villian's henchman.
+
+            string RobinsPhrase = default(string);
+            string PinkysPhrase = default(string);
+
+            Assert.AreEqual("Holy Code Exercise, Batman! Holy Code Exercise, Batman! Holy Code Exercise, Batman! ", RobinsPhrase);
+            Assert.AreEqual("Narf! Narf! Narf!", PinkysPhrase);
+        }
+
+        
+
     }
 }
