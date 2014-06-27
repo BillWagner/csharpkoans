11470546ccf223b746a4757196e128161b43fc6b
Ijwu <hmfarran1@hawkmail.hfcc.edu>
2014-06-27 16:19:28 -0400

    More changes to improve instruction and expectation clarity.

--- CSharpKoans/AboutInterfaces.cs
+++ CSharpKoans/AboutInterfaces.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace CSharpKoans
 {
+    /// <summary>
+    /// Interfaces enforce contracts. This is useful if you're using types
+    /// of varying specificity.
+    /// </summary>
     public  class AboutInterfaces : KoanContainer
     {
         public interface ICharacter
@@ -20,21 +24,22 @@ namespace CSharpKoans
             return rVal.ToString();
         }
 
+        /// <summary>
+        /// Interfaces make promises that you must fulfil
+        /// in your implementations.
+        /// </summary>
+        /// <instructions>
+        /// Implement the two sidekicks with their given catchphrases
+        /// and instantiate their respective variables.
+        /// </instructions>
         [Koan]
         public void InterfacesProvideCommonContracts()
         {
-            var Robin = new object();
-            var Pinky = new object();
+            var Robin = new object() as ICharacter;
+            var Pinky = new object() as ICharacter;
 
-            // Write code so that you can call RepeatCatchPhrase for both Robin and Tonnto
-            // create different classes for a super hero side kick,
-            // and a villian's henchman.
-
-            string RobinsPhrase = default(string);
-            string PinkysPhrase = default(string);
-
-            Assert.AreEqual("Holy Code Exercise, Batman! Holy Code Exercise, Batman! Holy Code Exercise, Batman! ", RobinsPhrase);
-            Assert.AreEqual("Narf! Narf! Narf!", PinkysPhrase);
+            Assert.AreEqual("Holy Code Exercise, Batman! Holy Code Exercise, Batman! Holy Code Exercise, Batman! ", RepeatCatchPhrase(Robin));
+            Assert.AreEqual("Narf! Narf! Narf!", RepeatCatchPhrase(Pinky));
         }
     }
 }
